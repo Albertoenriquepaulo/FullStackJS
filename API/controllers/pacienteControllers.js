676e41e4d1a42b5a649170a4fe760e6b52d096d2
Alberto Paulo <Albertoenriquepaulo@yahoo.es>
2020-01-12 18:08:47 +0100

    311. Obtener todos los registros

--- API/controllers/pacienteControllers.js
+++ API/controllers/pacienteControllers.js
@@ -1,15 +1,27 @@
 // Importamos el modelo
 const Paciente = require('../models/Paciente')
 
-// Cuando se crea un nuevo cliente
-exports.nuevoCliente = async (req, res, next) => {
+// Cuando se crea un nuevo paciente
+exports.nuevoPaciente = async (req, res, next) => {
     // Crear objeto de paceinte con datos de req.body
     const paciente = new Paciente(req.body);
 
-    //TODO : insertar en la BD
+    // Insertar en la BD
     try {
         await paciente.save();
         res.json({ mensaje: 'El cliente se agregó correctamente' });
+    } catch (error) {
+        console.log(error);
+        next();
+    }
+}
+
+// Obtiene todos los pacientes
+exports.obtenerPacientes = async (req, res, next) => {
+    // Obtener de la BD
+    try {
+        const pacientes = await Paciente.find({});
+        res.json(pacientes);
     } catch (error) {
         console.log(error);
         next();
--- API/routes/index.js
+++ API/routes/index.js
@@ -4,6 +4,11 @@ const pacienteController = require('../controllers/pacienteControllers')
 
 module.exports = function () {
     // Agrega nuevos pacientes via POST
-    router.post('/pacientes', pacienteController.nuevoCliente);
+    router.post('/pacientes', pacienteController.nuevoPaciente);
+
+
+    // Obtiene todos los registros de paciente de la DB
+    router.get('/pacientes', pacienteController.obtenerPacientes);
+
     return router;
 }
