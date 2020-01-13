050b1f79a5416e362976f23afa570ced78b0440d
Alberto Paulo <Albertoenriquepaulo@yahoo.es>
2020-01-13 16:39:05 +0100

    327. Finalizando la Cita

--- frontend/src/components/Cita.js
+++ frontend/src/components/Cita.js
@@ -1,7 +1,52 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { Link, withRouter } from 'react-router-dom';
 
-const Cita = () => {
-    return (<h1>Desde Cita Component</h1>);
+const Cita = (props) => {
+
+    if (!props.cita) {
+        props.history.push('/');
+        return null;
+    }
+
+    // Extraer por props
+    const { cita: { nombre, propietario, fecha, hora, telefono, sintomas } } = props; // Tambien se puede "const { cita } = props;" pero habria que colocar abajo {cita.nombre}
+    return (
+        <Fragment>
+            <h1 className="my-5">Nombre Cita {nombre}</h1>
+
+            <div className="container mt-5 py-5">
+                <div className="row">
+                    <div className="col-12 mb-5 d-flex justify-content-center">
+                        <Link to={'/'} href="" className="btn btn-success text-uppercase py-2 px-5 font-weigh-bold">Volver</Link>
+                    </div>
+
+                    <div className="col-md-8 mx-auto">
+                        <div className="list-group">
+                            <div className="p-5 list-group-item list-group-item-action flex-column align-items-center">
+                                <div className="d-flex w-100 justify-content-between mb-4">
+                                    <h3 className="mb-3">{nombre}</h3>
+                                    <small className="fecha-alta">
+                                        {fecha} / {hora}
+                                    </small>
+                                </div>
+                                <p className="mb-0">
+                                    {sintomas}
+                                </p>
+                                <div className="contacto py-3">
+                                    <p>Dueño: {propietario}</p>
+                                    <p>Teléfono: {telefono}</p>
+                                </div>
+
+                                <div className="d-flex">
+                                    <button type="button" className="text-uppercase py-2 px-5 font-weight-bold btn btn-danger col">Eliminar &times;</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </Fragment>
+    );
 }
 
-export default Cita;
+export default withRouter(Cita);
